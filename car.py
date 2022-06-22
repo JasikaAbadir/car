c5cd0df918a9b0816b05838a5286b24a8a3b4f2f
JasikaAbadir <sammyjessica05@gmail.com>
2022-06-22 20:02:52 +0100

    trying robot module instead of motor module

--- car.py
+++ car.py
@@ -1,4 +1,4 @@
-#test
+
 # Write your code here :-)
 #from gpiozero import Robot
 import RPi.GPIO as GPIO
@@ -9,13 +9,42 @@ from time import sleep
 #sleep(10)
 #Roberta.stop()
 
-from gpiozero import Motor
+# from gpiozero import Motor
+from gpiozero import Robot
+from bluedot import BlueDot
 
-MotorL = Motor(6, 13)
-MotorR = Motor(19, 26)
+bd = BlueDot()
+robot = Robot(left=(6, 13), right=(19, 26))
 
-MotorL.forward()
-MotorR.forward()
-sleep(5)
-MotorL.stop()
-MotorR.stop()
+#MotorL = Motor(6, 13)
+#MotorR = Motor(19, 26)
+
+def move(pos):
+  if pos.top:
+      #MotorL.forward()
+      #MotorR.forward()
+      robot.forward()
+  elif pos.bottom:
+      #MotorL.backward()
+      #MotorR.backward()
+      robot.backward()
+  elif pos.right:
+      robot.right()
+  elif pos.left:
+      robot.left()
+      
+      
+def stop():
+    robot.stop()
+
+bd.when_pressed = move
+bd.when_moved = move
+bd.when_released = stop
+      
+      
+
+#MotorL.forward()
+#MotorR.forward()
+#sleep(5)
+#MotorL.stop()
+#MotorR.stop()
