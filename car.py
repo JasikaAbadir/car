b95d6d30b13a45d021926776959b6ab9e2925aed
JasikaAbadir <sammyjessica05@gmail.com>
2022-06-22 19:08:24 +0100

    first commit

---
+++ car.py
@@ -0,0 +1,20 @@
+# Write your code here :-)
+#from gpiozero import Robot
+import RPi.GPIO as GPIO
+from time import sleep
+
+#Roberta = Robot((6,13),(19,26))
+#Roberta.forward(0.8)
+#sleep(10)
+#Roberta.stop()
+
+from gpiozero import Motor
+
+MotorL = Motor(6, 13)
+MotorR = Motor(19, 26)
+
+MotorL.forward()
+MotorR.forward()
+sleep(5)
+MotorL.stop()
+MotorR.stop()
